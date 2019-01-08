cbd27b7ac5fd99eb321b84cb202cbcad305918b6
dadi11 <dadi.sikorski@gmail.com>
2019-01-08 18:09:40 +0100

    XenonHD: unify overlays
    Change-Id: I4054a96faa1430d5295840c65cb326d586e9f9ab

--- accents/black/Android.mk
+++ accents/black/Android.mk
@@ -1,10 +1,10 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_RRO_THEME := LineageBlackAccent
+LOCAL_RRO_THEME := Accent-Black
 LOCAL_CERTIFICATE := platform
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
-LOCAL_PACKAGE_NAME := LineageBlackAccent
+LOCAL_PACKAGE_NAME := Accent-Black
 
 LOCAL_PRIVATE_PLATFORM_APIS := true
 

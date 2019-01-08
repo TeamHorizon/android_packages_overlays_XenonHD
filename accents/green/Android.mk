62857f77f31baf99046ccdee782ffc398f362cb3
dadi11 <dadi.sikorski@gmail.com>
2019-01-08 18:09:40 +0100

    XenonHD: unify overlays
    Change-Id: I4054a96faa1430d5295840c65cb326d586e9f9ab

--- accents/green/Android.mk
+++ accents/green/Android.mk
@@ -1,10 +1,11 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_RRO_THEME := LineageGreenAccent
+LOCAL_RRO_THEME := Accent-Green
 LOCAL_CERTIFICATE := platform
+LOCAL_SDK_VERSION := current
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
-LOCAL_PACKAGE_NAME := LineageGreenAccent
+LOCAL_PACKAGE_NAME := Accent-Green
 
 LOCAL_PRIVATE_PLATFORM_APIS := true
 
